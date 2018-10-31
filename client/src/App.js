f16f8d481a19a73728367d8907a5614b600ab9a8
erick casco <erick9c@gmail.com>
2018-10-30 19:00:52 -0500

    making changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import './orange.jpg';
 class App extends Component {
   render() {
     return (
+
+      
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
+      
+      <div className="App__Form">
+      <div className="PageSwitcher">
+
+        <a href="#" className="PageSwitcher__Item">Sign In</a>.
+        <a href="#" className="PageSwitcher__Item PageSwitcher__Item--Active">Sign Up</a>
       </div>
-    );
+
+      
+
+      <div className="FormCenter">
+        <form className="FormFields" onSubmit={this.handleSubmit}>
+          <div className="FormField">
+          <label className="FormField__Label" htmlFor="name">Full Name</label>
+          <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name"/>
+          </div>
+
+          <div className="FormField">
+          <label className="FormField__Label" htmlFor="name">Password</label>
+          <input type="text" id="password" className="FormField__Input" placeholder="Enter your password" name="password"/>
+          </div>
+
+          <div className="FormField">
+          <label className="FormField__Label" htmlFor="">E-mail Address</label>
+          <input type="text" id="email" className="FormField__Input" placeholder="Enter your email" name="email"/>
+          </div>
+
+
+          <div className="FormField">
+          <label className="FormField__CheckboxLabel">
+            <input className="FormField__Checkbox" type="checkbox" name="hasAgreed"/> I agree all statements in <a href="" className="FormField__TermsLink">terms of service</a>
+          </label>
+
+          </div>
+
+          <div className="FormField">
+            <button className="FormField__Button mr-20">Sign Up</button> <a href="#" className="FormField__Link">I'm already a member</a>
+          
+          </div>
+
+        </form>
+      
+      </div>
+      
+
+      
+        
+      
+      
+      </div>
+      </div>
+    
+      
+      );
   }
 }
 
