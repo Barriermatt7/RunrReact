dd87c67c3076a98d0a3f01f571e40f45936da97a
erick casco <erick9c@gmail.com>
2018-10-31 23:01:15 -0500

    making changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,24 @@
-import React, { Component } from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+//import About from "./pages/About";
+import Navbar from "./components/Navbar";
 
-import './App.css';
-import './orange.jpg';
-class App extends Component {
-  render() {
-    return (
+import Wrapper from "./components/Wrapper";
 
-      
-
-      <div className="App">
+const App = () => (
+  <Router>
+    <div>
+      <Navbar />
+      <Wrapper>
         
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+      </Wrapper>
       
-      <div className="App__Form">
-      <div className="PageSwitcher">
-
-        <a href="#" className="PageSwitcher__Item">Sign In</a>.
-        <a href="#" className="PageSwitcher__Item PageSwitcher__Item--Active">Sign Up</a>
-      </div>
-
-      
-
-      <div className="FormCenter">
-        <form className="FormFields" onSubmit={this.handleSubmit}>
-          <div className="FormField">
-          <label className="FormField__Label" htmlFor="name">Full Name</label>
-          <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name"/>
-          </div>
-
-          <div className="FormField">
-          <label className="FormField__Label" htmlFor="name">Password</label>
-          <input type="text" id="password" className="FormField__Input" placeholder="Enter your password" name="password"/>
-          </div>
-
-          <div className="FormField">
-          <label className="FormField__Label" htmlFor="">E-mail Address</label>
-          <input type="text" id="email" className="FormField__Input" placeholder="Enter your email" name="email"/>
-          </div>
-
-
-          <div className="FormField">
-          <label className="FormField__CheckboxLabel">
-            <input className="FormField__Checkbox" type="checkbox" name="hasAgreed"/> I agree all statements in <a href="" className="FormField__TermsLink">terms of service</a>
-          </label>
-
-          </div>
-
-          <div className="FormField">
-            <button className="FormField__Button mr-20">Sign Up</button> <a href="#" className="FormField__Link">I'm already a member</a>
-          
-          </div>
-
-        </form>
-      
-      </div>
-      
-
-      
-        
-      
-      
-      </div>
-      </div>
-    
-      
-      );
-  }
-}
+    </div>
+  </Router>
+);
 
 export default App;
